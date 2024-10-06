4aa3747fcc59422b0243540ac9a6b324e3279d83
onurkantarci <onurkntrc@gmail.com>
2024-10-07 00:21:54 +0300

    add validationpipe

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as session from 'express-session';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.useGlobalPipes(new ValidationPipe());
   app.use(
     session({
       secret: 'my-secret',
